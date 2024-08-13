37e3a7ecaa156d90f903b447808f50806a69675f
lpatel123 <47672178+lpatel123@users.noreply.github.com>
2024-08-13 14:39:30 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "Resource_group_name" {
+type = string
+    description = "resource group name"
+}
+variable "Existing_rg" {
+type = string
+    description = "resource group name"
+}
+variable "location" {
+    type = string
+    default = "eastus"
+}
+variable "vnet_name" {
+type = string
+    description = "resource group name"
+}
+variable "subnet_name" {
+type = string
+    description = "resource group name"
+}
+variable "Virtual_Machine_name" {
+type = string
+    description = "resource group name"
+}
+variable "VM_size" {
+type = string
+    description = "resource group name"
+}
+variable "admin_username" {
+type = string
+    description = "resource group name"
+}
+variable "admin_password" {
+type = string
+    description = "resource group name"
+}
